4457d5d59af55fbc5d6aeb14a538458604cb4ad1
EmilioChenf <che24841@uvg.edu.gt>
2025-01-19 22:54:59 -0600

    metodos impleemtado de interfaz

--- Licuadora.java
+++ Licuadora.java
@@ -1,3 +1,45 @@
-public class Licuadora {
-    
-}
+public class Licuadora implements Interfaz {
+
+    private boolean encendida;
+    private boolean llena;
+    private int velocidad;
+
+    public Licuadora() {
+        this.encendida = false;
+        this.llena = false;
+        this.velocidad = 0;
+    }
+
+    @Override
+    public void encender() {
+        if (!encendida) {
+            encendida = true;
+            System.out.println("Licuadora encendida.");
+        } else {
+            System.out.println("La licuadora ya está encendida.");
+        }
+    }
+
+    @Override
+    public void llenar() {
+        if (!llena) {
+            llena = true;
+            System.out.println("La licuadora se ha llenado.");
+        } else {
+            System.out.println("La licuadora ya está llena.");
+        }
+    }
+
+    @Override
+    public void aumentarVelocidad() {
+        if (!encendida) {
+            System.out.println("La licuadora está apagada. No puedes aumentar la velocidad.");
+        } else if (!llena) {
+            System.out.println("La licuadora está vacía. No puedes aumentar la velocidad.");
+        } else if (velocidad < 10) {
+            velocidad++;
+            System.out.println("Velocidad aumentada a: " + velocidad);
+        } else {
+            System.out.println("La velocidad ya está en su máximo nivel.");
+        }
+    }
