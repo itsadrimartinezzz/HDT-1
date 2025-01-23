fed2c5a6fec6501faa6e0ded9982de9a9aa5d93e
André Rodriguez <35317562+andro095@users.noreply.github.com>
2025-01-23 11:25:52 -0600

    Prueba unitaria con error

--- LicuadoraTest.java
+++ LicuadoraTest.java
@@ -1,1 +1,21 @@
-//aquí es donde van todos los test
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LicuadoraTest {
+
+    private Licuadora licuadora;
+
+    @BeforeEach
+    void setUp() {
+        licuadora = new Licuadora();
+    }
+    @Test
+    void testEncenderDosVecesErrorConExcepcion() {
+        licuadora.encender(); // Primera vez: encender con éxito.
+
+        // Segunda vez: se espera una excepción.
+        Exception exception = assertThrows(IllegalStateException.class, licuadora::encender);
+        assertEquals("La licuadora ya está encendida.", exception.getMessage());
+    }
+}
